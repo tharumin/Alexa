f9049d6f1e1569f908cd3fbc702ecb32da8a56f4
tharumin <94857655+tharumin@users.noreply.github.com>
2024-10-14 14:40:05 +0530

    Add files via upload

---
+++ plugins/pinterest .js
@@ -0,0 +1,15 @@
+const {cmd , commands} = require('../command')
+const {pinterest } = require( '@bochilteam/scraper')
+
+cmd({
+    pattern: "pinterest",
+    desc: "downlode pinterest",
+    category: "downlode",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*give me a title*\n- Ex : .pinterest anime girl")
+const json = await pinterest(q)
+
+await conn.sendMessage(from,{image: {url: json},caption: `- Your search result\n> ALEXA-MD`},{quoted: mek})
