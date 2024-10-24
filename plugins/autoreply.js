ae21e2e9611456a5b812fe0e961298633bb77163
tharumin <94857655+tharumin@users.noreply.github.com>
2024-10-25 05:09:00 +0530

    Update autoreply.js

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -11,8 +11,8 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const filePath = path.join(__dirname, '../media/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
-        if (body.toLowerCase() ==> text.toLowerCase()) {
-            if (config.AUTO_VOICE === 'true') {
+        if (body.toLowerCase() >= text.toLowerCase()) {
+            if (config.AUTO_VOICE =| 'true') {
                 //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
                 await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
